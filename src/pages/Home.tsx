188bc934645673049dbd3e61ac0f738864156edb
Oliver Ullman <oliverullman@gmail.com>
2021-12-16 13:13:54 -0300

    really shitty homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
-import { Text } from '@chakra-ui/layout';
+import { Box, Text } from '@chakra-ui/layout';
 import { Layout } from 'components/Layout/Layout';
 
 export const Home = () => (
   <Layout>
-    <Text color="white" m={10}>
-      Welcome! This is a very, very, very early version of AnyoneCanCode. Right
-      now we are missing a ton of important features. Our current focus is
-      building out our first course: JS Foundations. Click the Map button in the
-      upper left hand corner to get started.
-    </Text>
+    <Box mx={10} color="white">
+      <Text fontSize="3xl">
+        If you are not embarrassed by the first version of your product, you've
+        launched too late - Reid Hoffman
+      </Text>
+      <Box my={5}>---------------------</Box>
+      <Text>
+        Welcome! This is a very, very, very early version of AnyoneCanCode.
+        Right now we are missing a ton of important features. Our current focus
+        is building out our first course: JS Foundations. Click the Map button
+        in the upper left hand corner to get started.
+      </Text>
+    </Box>
   </Layout>
 );
