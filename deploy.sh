fa2b5b0155f5a92dc7cc9ace0eea058fd686f1f4
Oliver Ullman <oliverullman@gmail.com>
2021-12-18 17:54:15 -0300

    try again

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:oriooctopus/oriooctopus.github.io.git main
+git push -f https://github.com/oriooctopus/oriooctopus.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f https://github.com/oriooctopus/anyone-can-code.git master:gh-pages
